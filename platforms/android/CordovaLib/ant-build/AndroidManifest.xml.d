237dec7e1f57033c38bbae5fb166495b59986537
Flavio Ariano <fhariano@gmail.com>
2014-12-03 10:15:44 -0200

    Phonegap Test Capture
    Phonegap Test Capture

---
+++ platforms/android/CordovaLib/ant-build/AndroidManifest.xml.d
@@ -0,0 +1,2 @@
+C:\Phonegap\Projetos\capture\platforms\android\CordovaLib\ant-build\AndroidManifest.xml : \
+C:\Phonegap\Projetos\capture\platforms\android\CordovaLib\AndroidManifest.xml \
---
+++ platforms/android/ant-build/AndroidManifest.xml.d
@@ -0,0 +1,2 @@
+C:\Phonegap\Projetos\capture\platforms\android\ant-build\AndroidManifest.xml : \
+C:\Phonegap\Projetos\capture\platforms\android\AndroidManifest.xml \
